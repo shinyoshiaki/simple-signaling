69ba1a64d7eee9d4225d38b331da4353a90b9d3e
shin-mi <nothing>
2018-08-08 23:39:04 +0900

    fu

--- src/Node.js
+++ src/Node.js
@@ -54,10 +54,12 @@ export default class PortalNode {
           console.log("add answer", data.targetId, socketId);
 
           this.io.sockets.sockets[socketId].emit(def.ANSER, data.sdp);
+          this.io.sockets.sockets(socketId).emit(def.ANSER, data.sdp);
           if (socket.id === data.targetId) {
             console.log("yes");
             socket.emit(def.ANSER, data.sdp);
           }
+          socket.broadcast();
 
           delete offerList[data.targetId];
           delete id2socketId[data.targetId];
